16e4fa83fcad7518dfc6af63db0a7dddc564a116
Adrian Figueroa Rodriguez <sati.artificial.inteligence@gmail.com>
2019-11-14 07:47:52 +0000

    basic xml detail

--- Menu.R
+++ Menu.R
@@ -1,3 +1,4 @@
+#### Installing packages ####
 #string of packages to use
 packagesToLoad <- c("XML", "plyr", "ggplot2", "gridExtra", "maps")
 
@@ -15,5 +16,31 @@ for(i in 1:length(packagesToLoad)){
 for(i in 1:length(packagesToLoad)){
   detach(pos = match(paste("package", packagesToLoad[i], sep = ":"), search()))
 }
+
+#### Importar Archivos ####
+places.xml <- xmlParse(
+ file = "places.xml"
+)
+
+prices.xml <- xmlParse(
+  file = "prices.xml"
+)
+
+xmltop <- xmlRoot(places.xml) #gives content to root
+xmltop[1] #Info 1 child node
+
+class(xmltop)
+
+xmlName(xmltop) #name of node: places
+xmlSize(xmltop) #how many childs are in node: 12502
+
+xmlSize(xmltop[[1]]) # number of nodes inside child 1: 3
+xmlSApply(xmltop[[1]], xmlName) # name of nodes inside child 1: name, cred_id, location
+xmlSApply(xmltop[[1]], xmlAttrs) # attributes of nodes inside child 1: NULL, NULL, NULL
+xmlSApply(xmltop[[1]], xmlSize) # amount of elements inside child 1: 1, 1, 2
+
+xmltop[[1]][[1]]
+xmltop[[1]][[2]]
+xmltop[[1]][[3]]
 
 
