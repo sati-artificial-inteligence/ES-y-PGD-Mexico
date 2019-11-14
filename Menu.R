c9bbe200120aaf7901287704812f517bb3710416
Adrian Figueroa Rodriguez <sati.artificial.inteligence@gmail.com>
2019-11-14 07:30:14 +0000

    setting up packages

--- Menu.R
+++ Menu.R
@@ -1,1 +1,19 @@
-#Test text for push purposes
+#string of packages to use
+packagesToLoad <- c("XML", "plyr", "ggplot2", "gridExtra", "maps")
+
+#To load packages
+for(i in 1:length(packagesToLoad)){
+  if(!require(packagesToLoad[i], character.only = TRUE)){
+    install.packages(packagesToLoad, dependencies = TRUE)
+    require(packagesToLoad[i], character.only = TRUE)
+  }else{
+    require(packagesToLoad[i], character.only = TRUE)
+  }
+}
+
+#To detach packages
+for(i in 1:length(packagesToLoad)){
+  detach(pos = match(paste("package", packagesToLoad[i], sep = ":"), search()))
+}
+
+
